1886d0eff2f6235b9a5b5fd60121dc512856744d
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-03-12 23:11:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const button = document.querySelector('button')
 const popUpWrapper = document.querySelector('.popup-wrapper')
-const btnClose = document.querySelector('.popup-close')
-
 
 button.addEventListener('click', () => {
   popUpWrapper.style.display = 'block';
 })
 
-popUpWrapper.addEventListener('click', () => {
-  popUpWrapper.style.display = 'none'
+popUpWrapper.addEventListener('click', event => {
+  const classListElement = event.target.classList
+  console.log(classListElement);
 })
 
